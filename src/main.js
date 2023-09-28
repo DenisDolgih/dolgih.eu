30f8339c990b89a8ce1e0b71a8a86081d8644cd0
DenisDolgih <denis.dolgih@gmail.com>
2023-09-28 13:52:29 +0300

    change service-worker with vite-plugin-pwa

--- src/main.js
+++ src/main.js
@@ -13,16 +13,4 @@ const router = createRouter({
 
 app.use(router);
 app.mount('#app');
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        // Registration was successful
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(function(error) {
-        // Registration failed
-        console.error('Service Worker registration failed:', error);
-      });
-  }
   
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,woff,png,svg}']
+      }
+    })
   ],
 })
